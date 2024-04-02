77e76cab7460220931d7bf3fc693d17bce804606
matheusho <matheusho@icloud.com>
2024-04-02 08:13:11 -0300

    feat: add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
+import { Analytics } from "@vercel/analytics/react";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Hoxe - Held Today</title>
       </Head>
+      <Analytics />
       <Component {...pageProps} />
     </>
   );
