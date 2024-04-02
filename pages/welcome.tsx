e1914f53ac6f1c5d7297ce5048339beeb4eb9cc1
matheusho <matheusho@icloud.com>
2024-04-02 07:45:15 -0300

    feat: welcome page

--- pages/welcome.tsx
+++ pages/welcome.tsx
@@ -1,3 +1,52 @@
+import Link from "next/link";
+
+import { SquareDot } from "lucide-react";
+
+import { buttonVariants } from "@/components/ui/button";
+
 export default function Welcome() {
-    return 'Welcome';
+  return (
+    <div className="flex flex-col gap-2 min-h-screen">
+      <header className="py-10">
+        <div className="container px-4 md:px-6">
+          <nav className="flex items-center justify-between">
+            <div className="flex items-center space-x-2">
+              <SquareDot className="w-6 h-6" />
+              <span className="font-bold">hoxe</span>
+            </div>
+          </nav>
+        </div>
+      </header>
+      <main className="flex-1">
+        <section className="w-full pt-12 md:pt-24 lg:pt-32">
+          <div className="container px-4 md:px-6">
+            <div className="flex flex-col items-center justify-center space-y-2 text-center">
+              <div className="space-y-2">
+                <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl">Held Today</h1>
+                <p className="max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+                  Bring your day completed, manage your tasks as a dummie.
+                </p>
+              </div>
+              <div className="flex flex-col gap-2 min-[400px]:flex-row">
+                <Link href="/" className={buttonVariants()}>
+                  Get Started
+                </Link>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      {/* <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+        <p className="text-xs text-gray-500 dark:text-gray-400">© 2024 Acme Inc. All rights reserved.</p>
+        <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+          <Link className="text-xs hover:underline underline-offset-4" href="#">
+            Terms of Service
+          </Link>
+          <Link className="text-xs hover:underline underline-offset-4" href="#">
+            Privacy
+          </Link>
+        </nav>
+      </footer> */}
+    </div>
+  )
 }
